1346891163cfec39744bf06b004745c44a65e916
allonios <fareck.allony.mail@gmail.com>
2022-06-19 04:54:43 +0300

    feat: added search engine evaluation

--- ai_terms_semantic_search_engine/search_engine/tests.py
+++ ai_terms_semantic_search_engine/search_engine/tests.py
@@ -1,50 +1,182 @@
-import rdflib
+from typing import Any, Dict, List, Union
 
-graph = rdflib.Graph()
-
-graph.parse("../ignored/data/autoencoder/autoencoder2.owl")
-
-query1 = """
-SELECT ?a ?b
-WHERE {
-    ?a 
-    untitled-ontology-4:hasInput
-    <http://www.semanticweb.org/mohammed_almasry/ontologies/2021/5/untitled-ontology-4#feature_detectors_input> ;
-    
-    untitled-ontology-4:uses
-    <http://www.semanticweb.org/mohammed_almasry/ontologies/2021/5/untitled-ontology-4#en_autoencoder> ;
-    
-    untitled-ontology-4:description ?b .
-}
-"""
-
-query2 = """
-SELECT ?a ?b
-WHERE {
-    ?a untitled-ontology-4:learns ?b .    
-}
-"""
-
-query3 = """
-SELECT ?a ?b
-WHERE {
-    ?a <http://www.semanticweb.org/mohammed_almasry/ontologies/2021/5/untitled-ontology-4#weight> ?b .    
-}
-"""
-
-result = list(graph.query(query1))
+from pipline.handler import BaseHandler
+from query_builder.nl_query_processors import (
+    ExtractClosestTermsProcessor,
+    NLQueryTokenizer,
+    SparQLQueryBuilder,
+    TermsCombinationsProcessor,
+)
+from query_builder.ontology_processors import IterableExtractorProcessor
+from query_builder.states import QueryState
 
 
+class SearchEngineEvaluator:
+    def __init__(self, tests: List[Dict], search_engine: BaseHandler) -> None:
+        self.tests = tests
+        self.search_engine = search_engine
 
+    def run_search_engine(self):
+        results = []
+        for question in self.tests:
+            self.search_engine.run_processors(
+                QueryState(
+                    nl_query=question["question"],
+                    ontologies_base_dir="../ignored/data/autoencoder/",
+                )
+            )
+            result = self.search_engine.current_state.search_result
+            results.append(
+                {"question": question["question"], "result": result}
+            )
+        return results
 
-with open("result.txt", "w") as file:
-    file.write(
-        "\n".join(
-            map(
-                lambda res: f"{res.a}\n...\n{res.b}\n",
-                result
+    def get_test_case_by_question(self, question: str) -> Union[Dict, int]:
+        result = list(
+            filter(lambda test: test["question"] == question, self.tests)
+        )
+        if result:
+            return result[0]
+        else:
+            return -1
+
+    def search_for_result(self, question: str, result: Any) -> bool:
+        test_case = self.get_test_case_by_question(question)
+
+        for expected_result in test_case["result"]:
+            if isinstance(expected_result, str):
+                if expected_result in result:
+                    return True
+            else:
+                if result == expected_result:
+                    return True
+
+        return False
+
+    def evaluate(self) -> Dict:
+        search_results = self.run_search_engine()
+        results_existence = {}
+        for question in search_results:
+            results_existence[question["question"]] = []
+            for result in question["result"]:
+                results_existence[question["question"]].append(
+                    self.search_for_result(question["question"], result)
+                )
+
+        correct_predictions = len(
+            list(
+                filter(
+                    lambda result: any(results_existence[result]),
+                    results_existence,
+                )
             )
         )
-    )
+        accuracy = correct_predictions / len(self.tests)
+
+        return {"accuracy": accuracy}
 
 
+search_engine = BaseHandler(
+    processors=[
+        IterableExtractorProcessor(
+            get_attr_name="individuals",
+            set_attr_name="subjects",
+            processor_name="Subjects Extractor Processor",
+        ),
+        IterableExtractorProcessor(
+            get_attr_name="properties",
+            set_attr_name="predicates",
+            processor_name="Predicates Extractor Processor",
+        ),
+        NLQueryTokenizer(),
+        TermsCombinationsProcessor(window_size=4),
+        ExtractClosestTermsProcessor(
+            "subjects",
+            "in_query_subjects",
+            processor_name="Extract Closest Terms From " "Subjects Processor",
+            similarity_threshold=0.7,
+        ),
+        ExtractClosestTermsProcessor(
+            "predicates",
+            "in_query_predicates",
+            processor_name="Extract Closest Terms From "
+            "Predicates Processor",
+            similarity_threshold=0.7,
+            default="description",
+        ),
+        SparQLQueryBuilder(),
+    ],
+)
+
+evaluator = SearchEngineEvaluator(
+    tests=[
+        {
+            "case_description": "getting a straight forward attribute.",
+            "question": "is autoencoder supervised?",
+            "result": [False],
+        },
+        {
+            "case_description": "getting a complex attribute "
+            "(it will fail due to file structure).",
+            "question": "what is autoencoder loss function formula?",
+            "result": ["formula num 1 for testing sake"],
+        },
+        {
+            "case_description": "looking for implicit description.",
+            "question": "what is cnn autoencoder?",
+            "result": [
+                "Convolutional Autoencoders\nconvolutional neural networks "
+                "are far better suited than dense networks to work with "
+                "images. So if you want to build an autoencoder for images"
+            ],
+        },
+        {
+            "case_description": "looking for implicit description.",
+            "question": "tell me about the image datasets?",
+            "result": ["1-image_net : https://www.image-net.org/"],
+        },
+        {
+            "case_description": "playing with abbreviations.",
+            "question": "what is recurrent neural networks autoencoder?",
+            "result": [
+                "If you want to build an autoencoder for sequences, "
+                "such as time series or text (e.g., for unsupervised learning "
+                "or dimensionality reduction), then recurrent neural networks "
+                "may be better suited than dense networks. Building a "
+                "recurrent autoencoder is straightforward: the encoder is "
+                "typically a sequence-to-vector RNN which compresses the "
+                "input sequence down to a single vector. The decoder is a "
+                "vector-to-sequence RNN that does the reverse."
+            ],
+        },
+        {
+            "case_description": "looking for implicit description.",
+            "question": "what is rnn autoencoder?",
+            "result": [
+                "If you want to build an autoencoder for sequences, "
+                "such as time series or text (e.g., for unsupervised learning "
+                "or dimensionality reduction), then recurrent neural networks "
+                "may be better suited than dense networks. Building a "
+                "recurrent autoencoder is straightforward: the encoder is "
+                "typically a sequence-to-vector RNN which compresses the "
+                "input sequence down to a single vector. The decoder is a "
+                "vector-to-sequence RNN that does the reverse."
+            ],
+        },
+        {
+            "case_description": "getting a straight forward attribute.",
+            "question": "what is autoencoder optimizer?",
+            "result": [""],
+        },
+        {
+            "case_description": "getting a straight forward attribute.",
+            "question": "what does autoencoder learns?",
+            "result": [""],
+        },
+    ],
+    search_engine=search_engine,
+)
+
+metrics = evaluator.evaluate()
+
+print(metrics)
